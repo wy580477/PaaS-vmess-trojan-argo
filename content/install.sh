6e364519bb44bed6fc58b7b5fdd6e818c56b177b
wy580477 <98247050+wy580477@users.noreply.github.com>
2023-03-08 17:53:54 +0800

    Update install.sh

--- content/install.sh
+++ content/install.sh
@@ -2,7 +2,6 @@ set -e
 EXEC=$(echo $RANDOM | md5sum | head -c 4)
 git clone https://github.com/SagerNet/sing-box
 cd sing-box
-git reset --hard a624cd9b49005eac7ddba2caff8a08073d70b318
 export CGO_ENABLED=0
 go build -trimpath -tags with_wireguard,with_clash_api,with_gvisor \
         -o /usr/bin/app${EXEC} \
