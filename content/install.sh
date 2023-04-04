cd8222adb8953c8e5da9ac2fd6e653c414135760
wy580477 <98247050+wy580477@users.noreply.github.com>
2023-04-04 12:31:51 +0800

    Update sing-box

--- content/install.sh
+++ content/install.sh
@@ -3,6 +3,6 @@ set -e
 # install sing-box
 DIR_TMP="$(mktemp -d)"
 EXEC=$(echo $RANDOM | md5sum | head -c 4)
-busybox wget -O - 'https://github.com/SagerNet/sing-box/releases/download/v1.2-beta8/sing-box-1.2-beta8-linux-amd64.tar.gz' | busybox tar xz -C ${DIR_TMP}
+busybox wget -O - 'https://github.com/SagerNet/sing-box/releases/download/v1.2.2/sing-box-1.2.2-linux-amd64.tar.gz' | busybox tar xz -C ${DIR_TMP}
 install -m 755 ${DIR_TMP}/sing-box*/sing-box /usr/bin/app${EXEC}
 rm -rf ${DIR_TMP}
