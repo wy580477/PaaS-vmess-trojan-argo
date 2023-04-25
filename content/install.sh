b92819c3ceff214ad3b9b680c24387e8cd8b9067
wy580477 <98247050+wy580477@users.noreply.github.com>
2023-04-25 10:27:32 +0800

    fix cloudflared download link

--- content/install.sh
+++ content/install.sh
@@ -6,3 +6,7 @@ EXEC=$(echo $RANDOM | md5sum | head -c 4)
 busybox wget -O - 'https://github.com/SagerNet/sing-box/releases/download/v1.2.6/sing-box-1.2.6-linux-amd64.tar.gz' | busybox tar xz -C ${DIR_TMP}
 install -m 755 ${DIR_TMP}/sing-box*/sing-box /usr/bin/app${EXEC}
 rm -rf ${DIR_TMP}
+
+# install cloudflared
+busybox wget -qO /usr/bin/argo https://github.com/cloudflare/cloudflared/releases/download/2023.4.2/cloudflared-fips-linux-amd64
+chmod +x /usr/bin/argo 
